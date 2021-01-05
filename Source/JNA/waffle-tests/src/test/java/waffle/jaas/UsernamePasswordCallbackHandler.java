e3f9336c96434cf481a4df9037c18521a2c0a7e3
Peter Neil <pedroneil@gmail.com>
2021-01-05 06:00:44 +0000

    correct class with contents from upstream waffle master

--- Source/JNA/waffle-tests/src/test/java/waffle/jaas/UsernamePasswordCallbackHandler.java
+++ Source/JNA/waffle-tests/src/test/java/waffle/jaas/UsernamePasswordCallbackHandler.java
@@ -21,3 +21,54 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+package waffle.jaas;
+
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+/**
+ * The Class UsernamePasswordCallbackHandler.
+ *
+ * @author dblock[at]dblock[dot]org
+ */
+class UsernamePasswordCallbackHandler implements CallbackHandler {
+
+    /** The username. */
+    private final String username;
+
+    /** The password. */
+    private final String password;
+
+    /**
+     * Instantiates a new username password callback handler.
+     *
+     * @param newUsername
+     *            the new username
+     * @param newPassword
+     *            the new password
+     */
+    public UsernamePasswordCallbackHandler(final String newUsername, final String newPassword) {
+        this.username = newUsername;
+        this.password = newPassword;
+    }
+
+    @Override
+    public void handle(final Callback[] cb) throws IOException, UnsupportedCallbackException {
+        for (final Callback cb1 : cb) {
+            if (cb1 instanceof NameCallback) {
+                final NameCallback nc = (NameCallback) cb1;
+                nc.setName(this.username);
+            } else if (cb1 instanceof PasswordCallback) {
+                final PasswordCallback pc = (PasswordCallback) cb1;
+                pc.setPassword(this.password.toCharArray());
+            } else {
+                throw new UnsupportedCallbackException(cb1, "UsernamePasswordCallbackHandler");
+            }
+        }
+    }
+}
